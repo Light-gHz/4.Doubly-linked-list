777755b5cb0b7f69049f2807ce7cd0090a8a71b9
Light-gHz <belosh.sergo@mail.ru>
2024-06-09 20:58:47 +0300

    Add laba2.4

---
+++ laba 2.4/NodeList.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace laba_2._4
+{
+    internal class NodeList<T>
+    {
+        public Node<T> Head { get; set; }
+        public Node<T> Tail { get; set; }
+        public void AddInStart(int data)
+        {
+            if (Head == null)
+            {
+                Head = new Node<T>(data);
+                Tail = new Node<T>(data);
+
+            }
+            else
+            {
+                Node<T> current = Head;
+                current.Previous = new Node<T>(data);
+                Head = current.Previous;
+                Head.Next = current;
+                Node<T> current1 = Tail;
+                while (current1.Previous != null)
+                {
+                    current1 = current1.Previous;
+                }
+                current1.Previous = new Node<T>(data);
+                Node<T> current2 = Tail;
+                while (current2.Previous != null)
+                {
+                    current2 = current2.Previous;
+                }
+                current2.Next = current1;
+            }
+        }
+        public void AddInEnd(int data)
+        {
+            if (Tail == null)
+            {
+                Head = new Node<T>(data);
+                Tail = new Node<T>(data);
+            }
+            else
+            {
+                Node<T> current = Tail;
+                current.Next = new Node<T>(data);
+                Tail = current.Next;
+                Tail.Previous = current;
+                Node<T> current1 = Head;
+                while (current1.Next != null)
+                {
+                    current1 = current1.Next;
+                }
+                current1.Next = new Node<T>(data);
+                Node<T> current2 = Head;
+                while (current2.Next != null)
+                {
+                    current2 = current2.Next;
+                }
+                current2.Previous = current1;
+            }
+        }
+        public void WriteNodeListFromStart()
+        {
+            Console.WriteLine();
+            Node<T> current = Head;
+            if (current == null)
+            {
+                Console.WriteLine("В списке нету элементов");
+            }
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine("\n");
+        }
+        public void WriteNodeListFromEnd()
+        {
+            Console.WriteLine();
+            Node<T> current = Tail;
+            if (current == null)
+            {
+                Console.WriteLine("В списке нету элементов");
+            }
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Previous;
+            }
+            Console.WriteLine("\n");
+        }
+        public int FindAvg()
+        {
+            int avg = 0;
+            int count = 0;
+            Node<T> current = Head;
+            while (current != null)
+            {
+                avg += current.Data;
+                count++;
+                current = current.Next;
+            }
+            avg /= count;
+            return avg;
+        }
+        public void Sorting(int avg)
+        {
+            Node<T> current = Head;
+            while (current != null)
+            {
+                if (current.Data < avg)
+                {
+                    current.Data = avg;
+                }
+                current = current.Next;
+            }
+        }
+        public void FinishSorting()
+        {
+            if (Head == null)
+            {
+                Console.WriteLine("Нельзя отсортировать список без элементов");
+            }
+            else
+            {
+                int avg = FindAvg();
+                Sorting(avg);
+                Console.WriteLine("Список осортирован \n");
+            }
+        }
+    }
+}
---
+++ laba 2.4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NodeList<int> node = new NodeList<int>();
+            do
+            {
+                Console.WriteLine("Если хотите добавить елемент в список нажмите - 1");
+                Console.WriteLine("Если хотите вывести элементы из списка нажмите - 2");
+                Console.WriteLine("Если хотите отсортировать список чтобы все элементы ниже среднего заменили на среднее значение нажмите - 3");
+                Console.WriteLine("Если хотите выйти нажмите - любую другую кнопку");
+                string key = Console.ReadLine();
+                if (key == "1")
+                {
+                    Console.WriteLine("Если хотите добавить елемент в начало списока нажмите - 1");
+                    Console.WriteLine("Если хотите добавить елемент в конец списока нажмите - любую другую кнопку");
+                    string StartOrEnd = Console.ReadLine();
+                    if (StartOrEnd == "1")
+                    {
+                        Console.Write("Введите элемент : ");
+                        int element = Convert.ToInt32(Console.ReadLine());
+                        node.AddInStart(element);
+                    }
+                    else
+                    {
+                        Console.Write("Введите элемент : ");
+                        int element = Convert.ToInt32(Console.ReadLine());
+                        node.AddInEnd(element);
+                    }
+                }
+                else if (key == "2")
+                {
+                    Console.WriteLine("Если хотите вывести элементы с начала нажмите - 1");
+                    Console.WriteLine("Если хотите вывести элементы с конца нажмите - любую другую кнопку");
+                    string StartOrEnd = Console.ReadLine();
+                    if (StartOrEnd == "1")
+                    {
+                        node.WriteNodeListFromStart();
+                    }
+                    else
+                    {
+                        node.WriteNodeListFromEnd();
+                    }
+                }
+                else if (key == "3")
+                {
+                    node.FinishSorting();
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+        }
+    }
+}
